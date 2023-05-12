0f035668f33dece66b99fc6ffe1d55eb7e993ff5
Randy <randy708ochoa@gmail.com>
2023-05-12 14:48:13 -0600

    Titulo de los todos cambia si completaste todas las tareas

--- src/components/todoCounter/TodoCounter.js
+++ src/components/todoCounter/TodoCounter.js
@@ -2,9 +2,19 @@ import "./TodoCounter.css"
 
 function TodoCounter({ total, completed }) {
   return (
-    <h1>
-      Has completado {completed} de {total} TODOS
-    </h1>
+    <>
+      {total === completed && (
+          <h1>
+            !Felicidades has completado todas las tareas!
+          </h1>
+
+      )}
+      {total !== completed && (
+          <h1>
+            Has completado {completed} de {total} TODOS
+          </h1>
+      )}
+    </>
   );
 }
 
