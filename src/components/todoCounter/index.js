efcb8fb7953c480033392b2a3e67b476aff552f0
Randy <randy708ochoa@gmail.com>
2023-05-22 11:11:26 -0600

    TodoCounter ahora cambia entre 3 estados que se pueden renderizar

--- src/components/todoCounter/index.js
+++ src/components/todoCounter/index.js
@@ -7,19 +7,20 @@ export function TodoCounter() {
         totalTodos
     } = React.useContext(TodoContext)
 
+    let componeneteRenderizado
+
+    if(totalTodos === 0) {
+        componeneteRenderizado = <h1>ToDo List</h1>
+    } else if(totalTodos === completedTodos) {
+        componeneteRenderizado = <h1>!Felicidades has completado todas las tareas!</h1>
+
+    } else {
+        componeneteRenderizado = <h1>Has completado {completedTodos} de {totalTodos} TODOS</h1>
+    }
+
     return (
         <>
-            {totalTodos === completedTodos && (
-                <h1>
-                    !Felicidades has completado todas las tareas!
-                </h1>
-
-            )}
-            {totalTodos !== completedTodos && (
-                <h1>
-                    Has completado {completedTodos} de {totalTodos} TODOS
-                </h1>
-            )}
+            {componeneteRenderizado}
         </>
     );
 }
